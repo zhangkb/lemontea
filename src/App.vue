4100f404a80cdb079723383d4e66e0b343c1afe6
Zhangkb <zhangkb5210@163.com>
2018-03-22 15:55:32 +0800

    lemon

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+      <div class="logo">
+          <!-- <img src="./assets/logo.jpeg"> -->
+      </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+/*.hello {
+    background: url(../assets/images/lemon.jpeg) center no-repeat;
+}*/
+* {
+    padding: 0;
+    margin: 0;
+}
+html,
+body {
+    background-color: #fffff3;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+  /*background: url(./assets/images/lemon.jpeg) center no-repeat;*/
+}
+.logo {
+    width: 240px;
+    height: 240px;
+    overflow: hidden;
+    border-radius: 240px;
+    background: url(./assets/logo.jpg) center no-repeat;
+    background-size: 240px 240px;
+    margin: 40px auto;
+}
+</style>
