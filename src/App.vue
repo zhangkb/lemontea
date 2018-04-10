6ef897c57eca048bcaf9c3ecb7248e75edac2fb1
Zhangkb <zhangkb5210@163.com>
2018-04-10 16:59:16 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-      <div class="logo">
-          <!-- <img src="./assets/logo.jpeg"> -->
-      </div>
-
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -15,34 +12,12 @@ export default {
 </script>
 
 <style>
-
-/*.hello {
-    background: url(../assets/images/lemon.jpeg) center no-repeat;
-}*/
-* {
-    padding: 0;
-    margin: 0;
-}
-html,
-body {
-    background-color: #fffff3;
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /*margin-top: 60px;*/
-  /*background: url(./assets/images/lemon.jpeg) center no-repeat;*/
-}
-.logo {
-    width: 240px;
-    height: 240px;
-    overflow: hidden;
-    border-radius: 240px;
-    background: url(./assets/logo.jpg) center no-repeat;
-    background-size: 240px 240px;
-    margin: 40px auto;
+  margin-top: 60px;
 }
 </style>
